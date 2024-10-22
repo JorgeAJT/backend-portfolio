9e592b24fa00a174e1fbe8c817d3ff6464bf245b
Jorge Jiménez <154094092+JorgeAJT@users.noreply.github.com>
2024-10-22 20:55:43 +0200

    first commit rest APIs (#1)

---
+++ rest_APIs/src/database_connection.py
@@ -0,0 +1,14 @@
+import psycopg2
+import logging
+
+
+def database_connection() -> psycopg2.extensions.connection:
+    logging.info("Connecting with the db ...")
+
+    conn = psycopg2.connect(
+        dbname='postgres',
+        user='postgres',
+        password='1234',
+    )
+    logging.info("Successful connection!")
+    return conn
---
+++ rest_APIs/src/exercise1.py
@@ -0,0 +1,86 @@
+import uvicorn
+import logging
+from fastapi import FastAPI
+from rest_APIs.src.database_connection import database_connection
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+try:
+    conn = database_connection()
+
+    app = FastAPI()
+
+    @app.get('/meter_data/{meter_data_id}')
+    async def get_meter_data_by_id(meter_data_id: int):
+        cursor = conn.cursor()
+        try:
+            cursor.execute("SELECT * FROM meter_data WHERE meter_data_id = %s", (meter_data_id,))
+            value = cursor.fetchone()
+        except Exception as e:
+            logging.error(f"Error executing query: {e}")
+            value = None
+        finally:
+            cursor.close()
+
+        if value:
+            return {"status_code": 200, "body": {"message": "Returning meter_data row", "meter_data": value}}
+        else:
+            return {"status_code": 404, "body": {"message": "meter_data not found"}}
+
+    @app.get('/meter_data/')
+    async def get_meter_data_by_query_params(connection_ean_code: str, business_partner_id: str):
+        cursor = conn.cursor()
+        try:
+            cursor.execute("SELECT * FROM meter_data WHERE connection_ean_code = %s AND business_partner_id = %s", (connection_ean_code,business_partner_id))
+            value = cursor.fetchone()
+        except Exception as e:
+            logging.error(f"Error executing query: {e}")
+            value = None
+        finally:
+            cursor.close()
+
+        if value:
+            return {"status_code": 200, "body": {"message": "Returning meter_data row", "meter_data": value}}
+        else:
+            return {"status_code": 404, "body": {"message": "meter_data not found"}}
+
+    @app.get('/mandate_data/{mandate_id}')
+    async def get_mandate_data_by_id(mandate_id: int):
+        cursor = conn.cursor()
+        try:
+            cursor.execute("SELECT * FROM mandate_data WHERE mandate_id = %s", (mandate_id,))
+            value = cursor.fetchone()
+        except Exception as e:
+            logging.error(f"Error executing query: {e}")
+            value = None
+        finally:
+            cursor.close()
+
+        if value:
+            return {"status_code": 200, "body": {"message": "Returning mandate_data row", "mandate_data": value}}
+        else:
+            return {"status_code": 404, "body": {"message": "mandate_data not found"}}
+
+    @app.get('/mandate_data/')
+    async def get_mandate_data_by_query_params(business_partner_id: str, mandate_status: str):
+        cursor = conn.cursor()
+        try:
+            cursor.execute("SELECT * FROM mandate_data WHERE business_partner_id = %s AND mandate_status = %s", (business_partner_id,mandate_status))
+            value = cursor.fetchone()
+        except Exception as e:
+            logging.error(f"Error executing query: {e}")
+            value = None
+        finally:
+            cursor.close()
+
+        if value:
+            return {"status_code": 200, "body": {"message": "Returning mandate_data row", "mandate_data": value}}
+        else:
+            return {"status_code": 404, "body": {"message": "mandate_data not found"}}
+
+    if __name__ == "__main__":
+        uvicorn.run(app, port=8080)
+
+except Exception as e:
+    logging.error(f"Error: {e}")
+    raise e
