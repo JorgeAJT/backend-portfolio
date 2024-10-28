b06f699965b1969a91726b855c58949db8a8ce41
JorgeAJT <jorgeajtrueba@gmail.com>
2024-10-28 19:00:09 +0100

    added meter_readings to exercise 1

--- rest_APIs/src/exercise1.py
+++ rest_APIs/src/exercise1.py
@@ -78,6 +78,23 @@ try:
         else:
             return {"status_code": 404, "body": {"message": "mandate_data not found"}}
 
+    @app.get('/meter_readings/')
+    async def get_meter_reading_by_query_params(meter_number: str, account_id: str, energy_type: str):
+        cursor = conn.cursor()
+        try:
+            cursor.execute("SELECT * FROM meter_readings WHERE meter_number = %s AND account_id = %s AND energy_type = %s", (meter_number, account_id, energy_type))
+            value = cursor.fetchone()
+        except Exception as e:
+            logging.error(f"Error executing query: {e}")
+            value = None
+        finally:
+            cursor.close()
+
+        if value:
+            return {"status_code": 200, "body": {"message": "Returning meter_readings row", "meter_reading": value}}
+        else:
+            return {"status_code": 404, "body": {"message": "meter_reading not found"}}
+
     if __name__ == "__main__":
         uvicorn.run(app, port=8080)
 
